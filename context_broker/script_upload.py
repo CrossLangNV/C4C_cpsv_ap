c2f731ab918bd650b1bc10fe7291a53dad4e9b42
laurens <laurensmeeus93@gmail.com>
2021-12-07 09:48:19 +0100

    Make user-script

--- context_broker/script_upload.py
+++ context_broker/script_upload.py
@@ -400,31 +400,6 @@ def delete_all(debug=False):
     return
 
 
-def main_example():
-    delete_all()  # Clean slate.
-
-    filename = os.path.join(os.path.dirname(__file__), 'examples/demo_context_broker.jsonld')
-    parse_json_ld(filename)
-
-    if 0:
-        r = post_example()
-        try:
-            print(r.status_code)
-            print(r.content)
-            print(r.json())
-        except:
-            pass
-
-        r = delete_example()
-        try:
-            print(r.status_code)
-            print(r.content)
-            print(r.json())
-        except:
-            pass
-
-    print('Script has finished.')
-
-
 if __name__ == '__main__':
-    main_example()
+    filename = input("Enter filename of CPSV-AP in JSONLD format:\n")
+    parse_json_ld(filename)
