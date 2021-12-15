eacebf2fc4f25d7f884975de0ea21b977daf2270
laurens <laurensmeeus93@gmail.com>
2021-12-15 12:29:06 +0100

    Init pipeline
    Signed-off-by: Laurens <laurensmeeus93@gmail.com>

---
+++ scripts/example_html_extraction.py
@@ -0,0 +1,22 @@
+def main():
+    """
+    (for DEMO)
+    We want to extract relations from a webpage as found in the CPSV Application Profile.
+    Starting from a html page, we chunk the different sections, such that we can classify relevant sections.
+
+    The HTML page is first filtered to remove irrelevant content.
+    In the next stage, different relation extraction methods (these can be either rule-based or ML/NLP based) are applied.
+    To save the relations, they are put into an RDF.
+    """
+
+    # Get the HTML page
+
+    # Apply relation extraction
+
+    # Save in RDF
+
+    # Optional: Visualise results
+
+
+if __name__ == '__main__':
+    main()
