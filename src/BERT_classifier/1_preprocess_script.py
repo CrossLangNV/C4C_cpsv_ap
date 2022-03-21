2d4110fb6a8ca276a0606154b3777d736ded1988
infelane <laurensmeeus93@gmail.com>
2022-03-21 17:36:58 +0100

    Training on whole dataset after hyperparam optimalisation
    Signed-off-by: infelane <laurensmeeus93@gmail.com>

--- src/BERT_classifier/1_preprocess_script.py
+++ src/BERT_classifier/1_preprocess_script.py
@@ -16,5 +16,12 @@ if __name__ == '__main__':
         FILENAME_PREPROCESS_CONFIG = os.path.join(os.path.dirname(__file__),
                                                   "configuration_files_bert/preprocessing.config")
         main(FILENAME_PREPROCESS_CONFIG)
+    # Train on all data
+    elif 1:
+        DEFAULT_ARGS.update({"input_dir": os.path.join(DIR_SOURCE, "data/relation_extraction/only_train"),
+                             "preprocessed_data_dir": "./DATA/only_train_preprocessed_distilbert_base_uncased_cpsv_ap_relations",
+                             })
+
+        main_with_args(**DEFAULT_ARGS)
     else:
         main_with_args(**DEFAULT_ARGS)
