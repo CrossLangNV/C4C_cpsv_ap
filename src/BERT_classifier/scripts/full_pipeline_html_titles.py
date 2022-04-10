987071db034d1f491c73d02e867fe83dee1b4853
Laurens <laurensmeeus93@gmail.com>
2022-04-10 13:46:25 +0200

    titles classifier up and running with docker
    Signed-off-by: Laurens <laurensmeeus93@gmail.com>

--- src/BERT_classifier/scripts/full_pipeline_html_titles.py
+++ src/BERT_classifier/scripts/full_pipeline_html_titles.py
@@ -1,7 +1,7 @@
 import os
 from configparser import ConfigParser
 
-from BERT_classifier import preprocess_data_bert
+from BERT_classifier import preprocess_data_bert, train_bert
 
 
 class ConfigParserTitles(ConfigParser):
@@ -10,7 +10,7 @@ class ConfigParserTitles(ConfigParser):
         return self[section]
 
     @property
-    def preprocessing_configurations(self, section="PREPROCESSING_CONFIGURATIONS"):
+    def model_configurations(self, section="MODEL_CONFIGURATIONS"):
         return self[section]
 
     @property
@@ -38,27 +38,49 @@ class ConfigParserTitles(ConfigParser):
         return self.input_output.get(option)
 
     @property
-    def preprocessing_configurations_model_name(self,
-                                                option="MODEL_NAME_OR_PATH"
-                                                ) -> str:
-        return self.preprocessing_configurations.get(option)
+    def input_output_output_dir(self,
+                                option="OUTPUT_DIR"
+                                ) -> str:
+        return self.input_output.get(option)
+
+    @property
+    def model_configurations_model_name(self,
+                                        option="MODEL_NAME_OR_PATH"
+                                        ) -> str:
+        return self.model_configurations.get(option)
 
 
 def main(config: ConfigParserTitles,
-         preprocess: bool = True):
+         preprocess: bool = True,
+         train: bool = True):
     input_dir = config.input_output_input_dir
     preprocessed_data_dir = config.input_output_preprocessed_data_dir
     key_labels = config.input_output_labels
-    model_name = config.preprocessing_configurations_model_name
+    model_name = config.model_configurations_model_name
 
     # TODO use LABEL_INPUT
-    os.path.exists(input_dir)
-
+    # Preprocessing
     if preprocess:
         preprocess_data_bert.main_with_args(input_dir=input_dir,
                                             preprocessed_data_dir=preprocessed_data_dir,
                                             key_labels=key_labels,
                                             model_name=model_name)
+
+    output_dir = config.input_output_output_dir
+
+    if train:
+        # Training
+        train_bert.main_with_args(
+            preprocessed_data_dir=preprocessed_data_dir,
+            output_dir=output_dir,
+            model_name_or_path=model_name,
+            # batch_size=4,
+            # epochs: int,
+            # initial_lr: float,
+            # warm_up: bool,
+            # gpu: int,
+            save_model_each=1,
+        )
 
     return
 
@@ -73,4 +95,5 @@ if __name__ == '__main__':
     config.read(FILENAME_CONFIG)
 
     main(config,
-         preprocess=False)
+         preprocess=False,
+         train=True)
